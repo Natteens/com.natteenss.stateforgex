3f7a583d8f8900f56108aedbe2345c4c4e3c23f8
Nathan Miranda <ngamerandroid@gmail.com>
2025-04-11 03:46:40 -0300

    fix: state machine initial release

--- Runtime/Code/Core/StateMachine.cs
+++ Runtime/Code/Core/StateMachine.cs
@@ -67,7 +67,8 @@ namespace StateForgeX.Code.Core {
             return node;
         }
 
-        class StateNode {
+        class StateNode 
+        {
             public IState State { get; }
             public HashSet<ITransition> Transitions { get; }
             
